a4be76aa80f9e0640a1839c41815bad61c98cbf9
Jack Matthews <jackhkmatthews@gmail.com>
2021-12-24 00:05:02 +0000

    feat(document): create custom document

---
+++ next-frontend/pages/_document.tsx
@@ -0,0 +1,26 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            rel="preload"
+            href="/Arial Narrow.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin=""
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
